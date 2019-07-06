f95f04d8f57c72b4e4cca67fe268adba76993c6c
Daniyar Sariev <sariev.d4@gmail.com>
2019-07-06 21:26:40 +0600

    changin configuration

--- ApiService/src/main/java/com/dnr/apiservice/controller/PhotoController.java
+++ ApiService/src/main/java/com/dnr/apiservice/controller/PhotoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/upload")
+@RequestMapping("/photoUpload")
 public class PhotoController {
     @Autowired
     private PersonPhotoStorageService personPhotoStorageService;
